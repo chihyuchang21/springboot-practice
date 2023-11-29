d39eaa59bf7da89c53377fe14c37174c2454d28c
Chih-Yu Chang <chihyuchang21@gmail.com>
2023-11-29 20:45:48 +0800

    Initializing Spring Boot Project

---
+++ src/test/java/com/cychang/sprinbootmall/SprinbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cychang.sprinbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
